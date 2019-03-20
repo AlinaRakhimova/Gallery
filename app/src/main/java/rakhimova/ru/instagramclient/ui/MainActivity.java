4f7a58249f245a8cd9c173704caf8c6276b8dae1
AlinaRakhimova <rr.alina@yandex.ru>
2019-03-20 16:31:04 +0400

    исправление по BottomNavigation

--- app/src/main/java/rakhimova/ru/instagramclient/ui/MainActivity.java
+++ app/src/main/java/rakhimova/ru/instagramclient/ui/MainActivity.java
@@ -7,14 +7,20 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import butterknife.BindView;
 import rakhimova.ru.instagramclient.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
