96d11607e72a6a5664e6c087fd853be80d8282fe
AlinaRakhimova <rr.alina@yandex.ru>
2019-03-21 15:58:31 +0400

    Добавлена анимация перехода между фрагментами Добавлен переход общего элемента между фрагментами

--- app/src/main/java/rakhimova/ru/instagramclient/ui/MainActivity.java
+++ app/src/main/java/rakhimova/ru/instagramclient/ui/MainActivity.java
@@ -1,11 +1,9 @@
 package rakhimova.ru.instagramclient.ui;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 import butterknife.BindView;
 import rakhimova.ru.instagramclient.R;
@@ -15,34 +13,35 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.navigation)
     BottomNavigationView navigation;
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_sign_in:
+                loadFragment(AuthorizationFragment.newInstance());
+                return true;
+            case R.id.navigation_gallery:
+                loadFragment(GalleryFragment.newInstance());
+                return true;
+        }
+        return false;
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_sign_in:
-                    loadFragment(AuthorizationFragment.newInstance());
-                    return true;
-                case R.id.navigation_gallery:
-                    loadFragment(GalleryFragment.newInstance());
-                    return true;
-            }
-            return false;
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fl_content, AuthorizationFragment.newInstance())
+                    .commit();
         }
-    };
+    }
 
     private void loadFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
+                .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_in_right)
                 .replace(R.id.fl_content, fragment)
                 .commit();
     }
