b1a569b48955097085331b58ffa6a8f0520e5585
AlinaRakhimova <rr.alina@yandex.ru>
2019-04-02 17:09:51 +0400

    Создан ViewPager с двумя фрагментами Добавлен TabLayout для переключения между фрагментами

--- app/src/main/java/rakhimova/ru/instagramclient/ui/MainActivity.java
+++ app/src/main/java/rakhimova/ru/instagramclient/ui/MainActivity.java
@@ -1,48 +1,41 @@
 package rakhimova.ru.instagramclient.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import rakhimova.ru.instagramclient.R;
+import rakhimova.ru.instagramclient.ui.adapter.ViewPagerAdapter;
+import rakhimova.ru.instagramclient.ui.transformation.CubeOutDepthTransformation;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.navigation)
-    BottomNavigationView navigation;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = item -> {
-        switch (item.getItemId()) {
-            case R.id.navigation_sign_in:
-                loadFragment(AuthorizationFragment.newInstance());
-                return true;
-            case R.id.navigation_gallery:
-                loadFragment(GalleryFragment.newInstance());
-                return true;
-        }
-        return false;
-    };
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fl_content, AuthorizationFragment.newInstance())
-                    .commit();
-        }
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(GalleryFragment.newInstance(), String.valueOf(R.string.gallery));
+        adapter.addFragment(FavoriteFragment.newInstance(), String.valueOf(R.string.favorite));
+        viewPager.setAdapter(adapter);
+        viewPager.setPageTransformer(true, new CubeOutDepthTransformation());
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    private void loadFragment(Fragment fragment) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_in_right)
-                .replace(R.id.fl_content, fragment)
-                .commit();
-    }
 }
