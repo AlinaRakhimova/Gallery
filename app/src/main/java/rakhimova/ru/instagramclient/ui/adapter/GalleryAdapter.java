2feeff918bf862d9c1221d7d3ca1a1190f9b360a
AlinaRakhimova <rr.alina@yandex.ru>
2019-04-10 00:39:32 +0300

    Добавлена анимация Transition в адаптере (не работает)

--- app/src/main/java/rakhimova/ru/instagramclient/ui/adapter/GalleryAdapter.java
+++ app/src/main/java/rakhimova/ru/instagramclient/ui/adapter/GalleryAdapter.java
@@ -1,10 +1,17 @@
 package rakhimova.ru.instagramclient.ui.adapter;
 
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.design.chip.Chip;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Fade;
+import android.transition.TransitionManager;
+import android.transition.TransitionSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +23,8 @@ import rakhimova.ru.instagramclient.ui.entity.ItemPhoto;
 public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHolder> {
 
     private ArrayList<ItemPhoto> itemsPhoto;
+    private View view;
+    private View viewDetails;
 
     public GalleryAdapter(ArrayList<ItemPhoto> items) {
         itemsPhoto = items;
@@ -24,8 +33,10 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
+        view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_photo, parent, false);
+        viewDetails = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_photo_heart_detail, parent, false);
         return new ViewHolder(view);
     }
 
@@ -33,7 +44,24 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.titleView.setText(itemsPhoto.get(position).getTitle());
         holder.photoView.setImageResource(itemsPhoto.get(position).getImageUrl());
+        holder.favoriteView.setOnClickListener(v -> showHeartAnimation());
+    }
 
+    private void showHeartAnimation() {
+        ConstraintLayout constraintLayout = view.findViewById(R.id.root);
+        ConstraintLayout detailsLayout = viewDetails.findViewById(R.id.root);
+        ConstraintSet mainConstraint = new ConstraintSet();
+        ConstraintSet detailsConstraint = new ConstraintSet();
+        mainConstraint.clone(constraintLayout);
+        detailsConstraint.clone(detailsLayout);
+
+        TransitionSet set = new TransitionSet();
+        set.addTransition(new Fade());
+        set.setDuration(1200);
+        set.setInterpolator(new AccelerateInterpolator());
+
+        TransitionManager.beginDelayedTransition(constraintLayout, set);
+        detailsConstraint.applyTo(constraintLayout);
     }
 
     @Override
@@ -49,11 +77,13 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
     class ViewHolder extends RecyclerView.ViewHolder {
         private final ImageView photoView;
         private final TextView titleView;
+        private final Chip favoriteView;
 
         private ViewHolder(final View view) {
             super(view);
             titleView = view.findViewById(R.id.title);
             photoView = view.findViewById(R.id.content_photo);
+            favoriteView = view.findViewById(R.id.chip_favorite);
         }
     }
 
